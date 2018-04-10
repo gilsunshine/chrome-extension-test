3ac9827d1b3d6f6fed934895518acb786b9c6632
gilsunshine <gilsunshine@gmail.com>
2018-04-10 09:31:37 -0400

    removed unnecessary files

--- sketch.js
+++ sketch.js
@@ -5,7 +5,6 @@ var s = function(sketch) {
   sketch.setup = function() {
     document.body.style['userSelect'] = 'none';
     let h = document.body.clientHeight;
-    // let pg = sketch.createGraphics(sketch.windowWidth, sketch.windowHeight)
     let c = sketch.createCanvas(sketch.windowWidth, sketch.windowHeight);
     c.position(0, 0);
     c.style('pointer-events', 'none');
@@ -15,15 +14,11 @@ var s = function(sketch) {
       let star = new Star(sketch.random(0, sketch.windowWidth), sketch.random(0, sketch.windowHeight), sketch.random(5,20), sketch.random(100, 255), sketch.random(100, 255), sketch.random(100, 255), 100)
       stars.push(star)
     }
-    // stars.forEach(star => {
-    //   console.log(star)
-    // })
   }
 
   sketch.draw = function() {
     sketch.clear()
     stars.forEach(star => {
-      // star.checkParticles()
       star.particles.forEach(particle => {
         particle.show()
         particle.update()
@@ -45,7 +40,7 @@ var s = function(sketch) {
     this.numParticles = Math.floor(sketch.random(3, 7))
     this.particles = []
     this.color = Math.floor(sketch.random(0, 3))
-    }
+  }
 
     createParticles(){
       for(let i = 0; i < this.numParticles; i++){
@@ -53,14 +48,6 @@ var s = function(sketch) {
         this.particles.push(newParticle)
     }
   }
-
-  // checkParticles(){
-  //   this.particles.forEach(particle => {
-  //     if(particle.alpha < 0){
-  //       this.particles.splice(this.particles.indexOf(particle), 1)
-  //     }
-  //   })
-  // }
 }
 
 class Particle{
@@ -92,73 +79,28 @@ class Particle{
     this.alphaDecrease = 5
   }
 
-  show(){
-    sketch.noStroke()
-    sketch.fill(this.r, this.g, this.b, this.alpha)
-    sketch.ellipse(this.x, this.y, this.radius)
-  }
+    show(){
+      sketch.noStroke()
+      sketch.fill(this.r, this.g, this.b, this.alpha)
+      sketch.ellipse(this.x, this.y, this.radius)
+    }
 
-  update(){
-    // console.log(this.y)
-    // console.log("windowHeight")
-    // console.log(sketch.windowHeight)
-    if(this.y >= sketch.windowHeight - this.radius){
-      this.y = sketch.windowHeight - (this.radius/2)
-    }else{
-      sketch.push()
-      sketch.rotate(this.direction)
-      this.x += this.xSpeed
-      this.y += this.ySpeed
-      sketch.pop()
-      this.y -= this.gravity
-      // this.alpha -= this.alphaDecrease
-      if (this.xSpeed > 4){
-        this.xSpeed -= this.acceleration
+    update(){
+      if(this.y >= sketch.windowHeight - this.radius){
+        this.y = sketch.windowHeight - (this.radius/2)
+      }else{
+        sketch.push()
+        sketch.rotate(this.direction)
+        this.x += this.xSpeed
+        this.y += this.ySpeed
+        sketch.pop()
+        this.y -= this.gravity
+        if (this.xSpeed > 4){
+          this.xSpeed -= this.acceleration
+        }
       }
-      // if (this.ySpeed > 4){
-      //   this.ySpeed -= this.acceleration
-      // }
-
     }
   }
-}
-
-
-  // class Star{
-  //   constructor(x, y, radius, r, g, b, alpha){
-  //     this.x = x
-  //     this.y = y
-  //     this.radius = radius
-  //     this.r = r
-  //     this.g = g
-  //     this.b = b
-  //     this.alpha = 255
-  //     this.alphaMod = 1
-  //   }
-  //
-  //   show(){
-  //     sketch.noStroke()
-  //     sketch.fill(this.r, this.g, this.b, this.alpha)
-  //     sketch.ellipse(this.x, this.y, this.radius - (this.radius*.75))
-  //
-  //     sketch.noStroke()
-  //     sketch.fill(this.r, this.g, this.b, this.alpha * .75)
-  //     sketch.ellipse(this.x, this.y, this.radius - (this.radius*.5))
-  //
-  //     sketch.noStroke()
-  //     sketch.fill(255, this.alpha * .5)
-  //     sketch.ellipse(this.x, this.y, this.radius)
-  //   }
-  //
-  //   update(){
-  //     this.alphaMod = this.alphaMod % 255
-  //     if (this.alphaMod === 0){
-  //       this.alphaMod = 1
-  //     }
-  //     this.alpha = this.alphaMod
-  //     this.alphaMod += 1
-  //   }
-  // }
 };
 
 
